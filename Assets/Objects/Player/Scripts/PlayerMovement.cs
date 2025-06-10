ee3036bd4d842453b33cbb32d07bbb20c215ff33
bee0ne1 <brunogb728@gmail.com>
2025-06-10 15:52:27 -0300

    jetbrains

--- Assets/Objects/Player/Scripts/PlayerMovement.cs
+++ Assets/Objects/Player/Scripts/PlayerMovement.cs
@@ -1,7 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private InputControls actions;
+    private RigidBody2D rb;
+    public float speed;
+    
+
+    void Wake()
+    {
+        
+        actions = new InputControls();
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
